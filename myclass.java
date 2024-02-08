449a6287f665e76c89a8ac0237fa179847c73db3
kimcollo234 <150146602+kimcollo234@users.noreply.github.com>
2024-02-08 02:17:24 -0800

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,8 @@
+//java program
+//file.myClass java
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ parameter list.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is" + age);
+
+    }
+
+    public static void main(String[] args) {
+        myMethod("Joyy", 19); // Joyy is 19 years
+        myMethod("Kenny", 32);// Kenny is 32 years
+        myMethod("Collins", 21);// Collins is 21 years
+    }
+}
